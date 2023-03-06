3ee192f5521bf123f351a741fab0ac21c9167f26
timikapapua <119502130+timikapapua@users.noreply.github.com>
2023-03-06 13:17:32 +0900

    Add files via upload

---
+++ ganjilorgenap.php
@@ -0,0 +1,8 @@
+<?php
+
+$bilangan = 6;
+
+$keterangan = ($bilangan % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+
+echo "$keterangan" . PHP_EOL;
+ 
---
+++ switcstatment.php
@@ -0,0 +1,27 @@
+<?php
+
+$nilai ="c";
+
+switch ($nilai) {
+    case 'a':
+        echo "5.000".PHP_EOL;
+
+        break;
+        case 'b':
+        echo "5.300" .PHP_EOL;
+        break;
+        case 'c':
+        echo "6.000" .PHP_EOL;
+        break;
+        case 'd':
+        echo "7.000" .PHP_EOL;
+        break;
+        case 'e':
+        echo "9.500" .PHP_EOL;
+        break;
+
+        default:
+        echo "MUNGKIN ANDA KURANG WARAS !!!!" .PHP_EOL;
+
+        break;
+}
